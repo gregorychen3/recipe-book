57238e91c75d85d45e2066ea3e5db8b161ff30ff
Gregory Chen <gregorychen3@gmail.com>
2020-07-26 03:26:02 -0400

    theme.ts

---
+++ ui/src/theme.ts
@@ -0,0 +1,13 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export default createMuiTheme({
+  palette: {
+    primary: { main: "#009DDB" },
+    secondary: { main: "#F09000" },
+    error: { main: "#EC1600" },
+    warning: { main: "#EBA300" },
+    success: { main: "#00C78E" },
+    background: { default: "#171717", paper: "#1E262E" },
+    type: "dark",
+  },
+});
