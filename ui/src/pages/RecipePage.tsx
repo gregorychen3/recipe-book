1e4286d0e5be976c4405d72b6fafb3eb3cf1b137
Gregory Chen <gregorychen3@gmail.com>
2020-07-26 19:17:20 -0400

    render recipe name

--- ui/src/pages/RecipePage.tsx
+++ ui/src/pages/RecipePage.tsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { Redirect, useParams } from "react-router-dom";
 import { selectRecipe } from "../features/recipe/RecipeSlice";
 import ActionMenu from "../components/ActionMenu";
+import { Typography } from "@material-ui/core";
 
 export default function RecipePage() {
   const [isEditing, setIsEditing] = useState(false);
@@ -24,10 +25,13 @@ export default function RecipePage() {
   };
 
   return (
-    <ActionMenu
-      onDelete={handleDeleteClicked}
-      onEdit={isEditing ? undefined : handleEditClicked}
-      onSave={isEditing ? handleSaveClicked : undefined}
-    />
+    <>
+      <ActionMenu
+        onDelete={handleDeleteClicked}
+        onEdit={isEditing ? undefined : handleEditClicked}
+        onSave={isEditing ? handleSaveClicked : undefined}
+      />
+      <Typography variant="h6">{recipe.name.toUpperCase()}</Typography>
+    </>
   );
 }
