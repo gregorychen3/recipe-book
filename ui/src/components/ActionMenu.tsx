692d60db752fd7a1f54341efe8e0d04bf9eba073
Gregory Chen <gregorychen3@gmail.com>
2020-07-26 19:14:00 -0400

    style button menu

--- ui/src/components/ActionMenu.tsx
+++ ui/src/components/ActionMenu.tsx
@@ -1,9 +1,20 @@
-import { ButtonGroup } from "@material-ui/core";
+import { ButtonGroup, makeStyles } from "@material-ui/core";
 import IconButton from "@material-ui/core/IconButton";
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
 import EditIcon from "@material-ui/icons/Edit";
 import SaveIcon from "@material-ui/icons/Save";
 import React from "react";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 interface Props {
   onDelete: () => void;
@@ -11,21 +22,24 @@ interface Props {
   onSave?: () => void;
 }
 export default function ActionMenu({ onDelete, onEdit, onSave }: Props) {
+  const classes = useStyles();
   return (
-    <ButtonGroup variant="text" color="primary">
-      <IconButton size="small" onClick={onDelete}>
-        <DeleteOutlineIcon fontSize="small" color="error" />
-      </IconButton>
-      {onSave && (
-        <IconButton size="small" onClick={onSave}>
-          <SaveIcon fontSize="small" />
+    <div className={classes.root}>
+      <ButtonGroup variant="text" color="primary">
+        <IconButton size="small" onClick={onDelete}>
+          <DeleteOutlineIcon fontSize="small" color="error" />
         </IconButton>
-      )}
-      {onEdit && (
-        <IconButton size="small" onClick={onEdit}>
-          <EditIcon fontSize="small" />
-        </IconButton>
-      )}
-    </ButtonGroup>
+        {onSave && (
+          <IconButton size="small" onClick={onSave}>
+            <SaveIcon fontSize="small" />
+          </IconButton>
+        )}
+        {onEdit && (
+          <IconButton size="small" onClick={onEdit}>
+            <EditIcon fontSize="small" />
+          </IconButton>
+        )}
+      </ButtonGroup>
+    </div>
   );
 }
