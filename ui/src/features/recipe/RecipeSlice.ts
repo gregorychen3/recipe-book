512b886e715388b21bf7d9b39b40837d87d24f9e
Gregory Chen <gregorychen3@gmail.com>
2020-07-26 13:01:35 -0400

    add fetch recipes to redux

--- ui/src/features/recipe/RecipeSlice.ts
+++ ui/src/features/recipe/RecipeSlice.ts
@@ -1,9 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { IRecipeModel } from "../../../../src/db/recipe";
+import apiClient from "../../apiClient";
+import { RootState } from "../../app/store";
 
+const fetchRecipes = createAsyncThunk("users/fetchRecipes", async () => {
+  const recipes = await apiClient.fetchRecipes();
+  return recipes.data;
+});
+
+interface State {
+  recipes: IRecipeModel[];
+}
+const initialState: State = { recipes: [] };
 export const recipeSlice = createSlice({
   name: "recipe",
-  initialState: {},
+  initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchRecipes.fulfilled, (state, action) => {
+      state.recipes = action.payload;
+    });
+  },
 });
 
 export default recipeSlice.reducer;
+
+export const selectCount = (state: RootState) => state.recipe.recipes;
