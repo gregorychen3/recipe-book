5bc3a725d0c0f2d5cd19c173f71136fca5161add
Gregory Chen <gregorychen3@gmail.com>
2020-07-25 22:05:04 -0400

    modify imports

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-import * as express from "express";
-const app = express();
-app.get("/", (req, res) => {
+import express from "express";
+
+const server = express();
+
+server.get("/", (req, res) => {
   res.send("Hello World");
 });
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running in http://localhost:${PORT}`);
 });
