d25b765dcb1aa9ae43fceee876b41eddc7f963c3
Gregory Chen <gregorychen3@gmail.com>
2020-07-26 00:52:57 -0400

    types

---
+++ src/db/recipe.ts
@@ -0,0 +1,28 @@
+import { Document, Model, model, Schema } from "mongoose";
+import { CourseValues, CuisineValues, IRecipe } from "../types";
+
+export interface IRecipeModel extends IRecipe, Document {}
+
+export const RecipeSchema = new Schema({
+  name: { type: String, required: true },
+  course: { type: String, enum: CourseValues, required: true },
+  cuisine: { type: String, enum: CuisineValues, required: true },
+  servings: { type: Number, required: true },
+  ingredients: {
+    type: [
+      {
+        name: { type: String, required: true },
+        qty: { type: Number, required: false },
+        unit: { type: String, required: false },
+      },
+    ],
+    required: true,
+  },
+  instructions: { type: [String], required: true },
+  sources: { type: [String], required: true },
+});
+
+export const Recipe: Model<IRecipeModel> = model<IRecipeModel>(
+  "Recipe",
+  RecipeSchema
+);
---
+++ src/types.ts
@@ -0,0 +1,37 @@
+export const CourseValues = [
+  "antipasti",
+  "primi",
+  "secondi",
+  "dolci",
+  "contorni",
+  "sauces",
+  "beverages",
+  "other",
+] as const;
+type ICourse = typeof CourseValues[number];
+
+export const CuisineValues = [
+  "italian",
+  "anglophone",
+  "mediterranean",
+  "french",
+  "asian",
+  "other",
+];
+type ICuisine = typeof CuisineValues[number];
+
+export interface IIngredient {
+  name: string;
+  qty?: number;
+  unit?: string;
+}
+
+export interface IRecipe {
+  name: string;
+  course: ICourse;
+  cuisine: ICuisine;
+  servings: number;
+  ingredients: IIngredient[];
+  instructions: string[];
+  sources: string[];
+}
