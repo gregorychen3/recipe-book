9fa716839ab80f7de0cf90ba1b59ded56ed41d2f
Gregory Chen <gregorychen3@gmail.com>
2020-07-26 13:51:38 -0400

    get group by search

--- ui/src/pages/RecipesPage.tsx
+++ ui/src/pages/RecipesPage.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 import { fetchRecipes, selectRecipes } from "../features/recipe/RecipeSlice";
+
+const GroupByValues = ["course", "cuisine", "alphabetical"] as const;
+type GroupBy = typeof GroupByValues[number];
 
 export default function RecipesPage() {
   const d = useDispatch();
@@ -10,9 +14,23 @@ export default function RecipesPage() {
 
   const recipes = useSelector(selectRecipes);
 
+  const { search } = useLocation();
+  const getGroupBy = (): GroupBy => {
+    const groupBy = new URLSearchParams(search).get("groupBy");
+    switch (groupBy) {
+      case "course":
+      case "cuisine":
+      case "alphabetical":
+        return groupBy;
+      default:
+        return "course";
+    }
+  };
+
   return (
     <>
       <div>Recipes Page</div>
+      <div>{getGroupBy()}</div>
       <pre>{JSON.stringify(recipes, null, 2)}</pre>
     </>
   );
