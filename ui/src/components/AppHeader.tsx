a34b4f20103f9038c909be1cb5df1b0970f18a45
Gregory Chen <gregorychen3@gmail.com>
2020-07-26 03:20:52 -0400

    github icon

--- ui/src/components/AppHeader.tsx
+++ ui/src/components/AppHeader.tsx
@@ -1,10 +1,9 @@
 import AppBar from "@material-ui/core/AppBar";
-import Badge from "@material-ui/core/Badge";
 import IconButton from "@material-ui/core/IconButton";
 import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import NotificationsIcon from "@material-ui/icons/Notifications";
+import GitHubIcon from "@material-ui/icons/GitHub";
 import React from "react";
 
 const useStyles = makeStyles((theme) => ({
@@ -36,9 +35,7 @@ export default function Dashboard() {
           Greg and Ally's Recipe Book
         </Typography>
         <IconButton color="inherit">
-          <Badge badgeContent={4} color="secondary">
-            <NotificationsIcon />
-          </Badge>
+          <GitHubIcon />
         </IconButton>
       </Toolbar>
     </AppBar>
