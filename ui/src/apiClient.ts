3ffe7d8330bc2385001d315d2864e6461dc4d354
Gregory Chen <gregorychen3@gmail.com>
2020-07-26 12:49:42 -0400

    add api client

---
+++ ui/src/apiClient.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosPromise } from "axios";
+import { IRecipeModel } from "../../src/db/recipe";
+import { IRecipe } from "../../src/types";
+
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+const apiClient = {
+  createRecipe: (recipe: IRecipe): AxiosPromise<IRecipeModel> => {
+    const path = `/api/recipes`;
+    return axios.post(path, recipe);
+  },
+  fetchRecipes: (): AxiosPromise<IRecipeModel[]> => {
+    const path = `/api/recipes`;
+    return axios.get(path);
+  },
+  fetchRecipe: (recipeId: string): AxiosPromise<IRecipeModel> => {
+    const path = `/api/recipes/${recipeId}`;
+    return axios.get(path);
+  },
+  updateRecipe: (id: string, recipe: IRecipe): AxiosPromise<IRecipeModel> => {
+    const path = `/api/recipes/${id}`;
+    return axios.post(path, recipe);
+  },
+  deleteRecipe: (recipeId: string): AxiosPromise<{ _id: string }> => {
+    const path = `/api/recipes/${recipeId}`;
+    return axios.delete(path);
+  },
+};
+
+export default apiClient;
