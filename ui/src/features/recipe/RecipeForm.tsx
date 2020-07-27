6438566b8685d8b9d6fbbc09a49b25325873b3c2
Gregory Chen <gregorychen3@gmail.com>
2020-07-27 19:17:53 -0400

    formik mui example

--- ui/src/features/recipe/RecipeForm.tsx
+++ ui/src/features/recipe/RecipeForm.tsx
@@ -1,9 +1,51 @@
 import React from "react";
 import { IRecipe } from "../../../../src/types";
+import { Formik, Form, Field } from "formik";
+import { Button, LinearProgress } from "@material-ui/core";
+import { TextField } from "formik-material-ui";
 
 interface Props {
   recipe: IRecipe;
 }
 export default function RecipeForm({ recipe }: Props) {
-  return <></>;
+  return (
+    <Formik
+      initialValues={recipe}
+      validate={(values) => {
+        return {};
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log(values);
+        setSubmitting(false);
+      }}
+    >
+      {({ submitForm, isSubmitting }) => (
+        <Form>
+          <Field
+            component={TextField}
+            name="email"
+            type="email"
+            label="Email"
+          />
+          <br />
+          <Field
+            component={TextField}
+            type="password"
+            label="Password"
+            name="password"
+          />
+          {isSubmitting && <LinearProgress />}
+          <br />
+          <Button
+            variant="contained"
+            color="primary"
+            disabled={isSubmitting}
+            onClick={submitForm}
+          >
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
 }
