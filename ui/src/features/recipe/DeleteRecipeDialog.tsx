57d2e2f011d541e8b5409b3fa5418babb8907b1b
Gregory Chen <gregorychen3@gmail.com>
2020-07-30 13:10:07 -0400

    delete dialog partial impl

--- ui/src/features/recipe/DeleteRecipeDialog.tsx
+++ ui/src/features/recipe/DeleteRecipeDialog.tsx
@@ -1,3 +1,35 @@
+import { Box } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
 import React from "react";
 
-export default function DeleteRecipeDialog() {}
+export interface DeleteRecipeDialogProps {
+  recipeId?: string;
+  onClose: () => void;
+}
+export default function DeleteRecipeDialog({ recipeId, onClose }: DeleteRecipeDialogProps) {
+  return (
+    <Dialog open={!!recipeId} onClose={onClose}>
+      <DialogTitle>Delete Recipe</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          To subscribe to this website, please enter your email address here. We will send updates occasionally.
+        </DialogContentText>
+        <TextField autoFocus margin="dense" id="name" label="Email Address" type="email" fullWidth />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="primary">
+          Cancel
+        </Button>
+        <Button onClick={onClose} color="primary">
+          <Box color="error.main">Delete</Box>
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
