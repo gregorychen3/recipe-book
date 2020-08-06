a8f00cfd6d2fae6efc40a6ac25962871723c119a
Gregory Chen <gregorychen3@gmail.com>
2020-08-06 17:19:41 -0400

    add admin emails as env

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import { NextFunction, Request, Response } from "express";
 import HttpException from "../errors";
 
+const ADMIN_EMAILS: string[] = process.env.ADMIN_EMAILS ? JSON.parse(process.env.ADMIN_EMAILS) : [];
+
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
   const tokenId = req.header("Authorization");
   if (!tokenId) {
@@ -11,8 +13,7 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const resp = await axios.get(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${tokenId}`);
     const decoded = resp.data;
-    console.log(decoded);
-    next();
+    return ADMIN_EMAILS.includes(decoded.email) ? next() : next(new HttpException(401, `Unauthorized: not admin user`));
   } catch (e) {
     return next(new HttpException(401, `Unauthorized: decoding tokenId: ${e.message}`));
   }
