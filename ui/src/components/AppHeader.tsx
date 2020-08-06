a95985e34e07e8ac54384efb3f5addc859edbd2e
Gregory Chen <gregorychen3@gmail.com>
2020-08-06 17:32:26 -0400

    move oauth avatar to own component

--- ui/src/components/AppHeader.tsx
+++ ui/src/components/AppHeader.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, LinearProgress, Menu, MenuItem } from "@material-ui/core";
+import { Button, LinearProgress, Menu, MenuItem } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import IconButton from "@material-ui/core/IconButton";
 import { makeStyles } from "@material-ui/core/styles";
@@ -6,15 +6,12 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import RestaurantIcon from "@material-ui/icons/Restaurant";
-import clsx from "clsx";
 import React, { useState } from "react";
-import GoogleLogin, { GoogleLoginResponse, GoogleLoginResponseOffline, GoogleLogout } from "react-google-login";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Link as RouterLink, useHistory } from "react-router-dom";
-import { toast } from "react-toastify";
 import { selectShowLoading } from "../app/apiSlice";
-import { clearUserTokenId, loadUserTokenId, selectUserTokenId } from "../app/userSlice";
 import { GroupBy } from "../pages/RecipesPage";
+import OauthAvatar from "./OauthAvatar";
 
 const browseMenuOpts: { label: string; value: GroupBy }[] = [
   { label: "By Course", value: "course" },
@@ -26,14 +23,11 @@ const useStyles = makeStyles((theme) => ({
   root: { flexGrow: 1 },
   menuButton: { marginRight: theme.spacing(2) },
   title: { flexGrow: 1, cursor: "pointer" },
-  avatar: { width: theme.spacing(3), height: theme.spacing(3) },
-  avatarActive: { backgroundColor: theme.palette.primary.main },
 }));
 
 export default function AppHeader() {
   const classes = useStyles();
   const history = useHistory();
-  const d = useDispatch();
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
@@ -42,27 +36,6 @@ export default function AppHeader() {
   const handleMenu = (e: React.MouseEvent<HTMLElement>) => setAnchorEl(e.currentTarget);
   const handleClose = () => setAnchorEl(null);
   const navToHome = () => history.push("/");
-
-  const user = useSelector(selectUserTokenId);
-  const handleLoginSuccess = (resp: GoogleLoginResponse | GoogleLoginResponseOffline) => {
-    if (resp.code) {
-      // TODO : if responseType is 'code', callback will return the authorization code that can be used to retrieve a refresh token from the server.
-      resp = resp as GoogleLoginResponseOffline;
-      return;
-    }
-    toast.success("Login succeeded");
-    d(loadUserTokenId((resp as GoogleLoginResponse).tokenId));
-  };
-  const handleLoginFailure = (err: any) => {
-    d(clearUserTokenId());
-    toast.error(`Failed to login: ${err.error}`);
-  };
-  const handleLogoutSuccess = () => {
-    d(clearUserTokenId());
-    toast.success("Logout succeeded");
-  };
-  const handleLogoutFailure = () => toast.error("Failed to logout");
-
   const handleGroupByChanged = (groupBy: GroupBy) => {
     history.push(`/recipes?groupBy=${groupBy}`);
     handleClose();
@@ -101,33 +74,7 @@ export default function AppHeader() {
         <Button color="inherit" component={RouterLink} to="/about">
           About
         </Button>
-        {user ? (
-          <GoogleLogout
-            clientId="733241561721-4u35j8dtjmkisfs479m9an9f6p6tep1s.apps.googleusercontent.com"
-            buttonText="Logout"
-            onLogoutSuccess={handleLogoutSuccess}
-            onFailure={handleLogoutFailure}
-            render={(renderProps) => (
-              <IconButton onClick={renderProps.onClick} disabled={renderProps.disabled}>
-                <Avatar className={clsx(classes.avatar, classes.avatarActive)} />
-              </IconButton>
-            )}
-          />
-        ) : (
-          <GoogleLogin
-            clientId="733241561721-4u35j8dtjmkisfs479m9an9f6p6tep1s.apps.googleusercontent.com"
-            buttonText="Login"
-            onSuccess={handleLoginSuccess}
-            onFailure={handleLoginFailure}
-            isSignedIn
-            cookiePolicy="single_host_origin"
-            render={(renderProps) => (
-              <IconButton onClick={renderProps.onClick} disabled={renderProps.disabled}>
-                <Avatar className={classes.avatar} />
-              </IconButton>
-            )}
-          />
-        )}
+        <OauthAvatar />
       </Toolbar>
       <LinearProgress hidden={!showLoading} />
     </AppBar>
---
+++ ui/src/components/OauthAvatar.tsx
@@ -0,0 +1,72 @@
+import { Avatar } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import React from "react";
+import GoogleLogin, { GoogleLoginResponse, GoogleLoginResponseOffline, GoogleLogout } from "react-google-login";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { clearUserTokenId, loadUserTokenId, selectUserTokenId } from "../app/userSlice";
+
+// client id for local development
+// const clientId = "733241561721-4u35j8dtjmkisfs479m9an9f6p6tep1s.apps.googleusercontent.com";
+
+// client id for prod
+const clientId = "733241561721-n27hobb24p9hak87q92kq90fmuobidhj.apps.googleusercontent.com";
+
+const useStyles = makeStyles((theme) => ({
+  avatar: { width: theme.spacing(3), height: theme.spacing(3) },
+  avatarActive: { backgroundColor: theme.palette.primary.main },
+}));
+
+export default function OauthAvatar() {
+  const classes = useStyles();
+  const d = useDispatch();
+
+  const user = useSelector(selectUserTokenId);
+  const handleLoginSuccess = (resp: GoogleLoginResponse | GoogleLoginResponseOffline) => {
+    if (resp.code) {
+      // TODO : if responseType is 'code', callback will return the authorization code that can be used to retrieve a refresh token from the server.
+      resp = resp as GoogleLoginResponseOffline;
+      return;
+    }
+    d(loadUserTokenId((resp as GoogleLoginResponse).tokenId));
+  };
+  const handleLoginFailure = (err: any) => {
+    d(clearUserTokenId());
+    toast.error(`Failed to login: ${err.error}`);
+  };
+  const handleLogoutSuccess = () => {
+    d(clearUserTokenId());
+    toast.success("Logout succeeded");
+  };
+  const handleLogoutFailure = () => toast.error("Failed to logout");
+
+  return user ? (
+    <GoogleLogout
+      clientId={clientId}
+      buttonText="Logout"
+      onLogoutSuccess={handleLogoutSuccess}
+      onFailure={handleLogoutFailure}
+      render={(renderProps) => (
+        <IconButton onClick={renderProps.onClick} disabled={renderProps.disabled}>
+          <Avatar className={clsx(classes.avatar, classes.avatarActive)} />
+        </IconButton>
+      )}
+    />
+  ) : (
+    <GoogleLogin
+      clientId={clientId}
+      buttonText="Login"
+      onSuccess={handleLoginSuccess}
+      onFailure={handleLoginFailure}
+      isSignedIn
+      cookiePolicy="single_host_origin"
+      render={(renderProps) => (
+        <IconButton onClick={renderProps.onClick} disabled={renderProps.disabled}>
+          <Avatar className={classes.avatar} />
+        </IconButton>
+      )}
+    />
+  );
+}
