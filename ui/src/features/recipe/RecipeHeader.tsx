71e7cae42b0a5b7499844456f42974c9ed5b62bc
Gregory Chen <gregorychen3@gmail.com>
2020-08-06 12:12:06 -0400

    refactor recipe header

--- ui/src/features/recipe/RecipeHeader.tsx
+++ ui/src/features/recipe/RecipeHeader.tsx
@@ -1,15 +1,6 @@
-import { makeStyles, Typography } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import React from "react";
 import ActionMenu from "../../components/ActionMenu";
-
-const useStyles = makeStyles((theme) => ({
-  header: {
-    display: "flex",
-    justifyContent: "space-between",
-    margin: theme.spacing(2),
-  },
-  hidden: { visibility: "hidden" },
-}));
 
 interface Props {
   title: string;
@@ -18,12 +9,19 @@ interface Props {
   disableSave?: boolean;
 }
 export default function RecipeHeader({ title, onDelete, onEdit, disableSave }: Props) {
-  const classes = useStyles();
   return (
-    <div className={classes.header}>
-      <ActionMenu disableSave className={classes.hidden} />
-      <Typography variant="h4">{title}</Typography>
-      <ActionMenu onDelete={onDelete} onEdit={onEdit} disableSave={!!disableSave} />
-    </div>
+    <Grid container direction="row" justify="space-between" alignItems="center" spacing={2}>
+      <Grid item xs={12} sm={12} md={2} />
+      <Grid item xs={12} sm={12} md={8}>
+        <Typography variant="h4" align="center">
+          {title}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={12} md={2}>
+        <Typography align="center">
+          <ActionMenu onDelete={onDelete} onEdit={onEdit} disableSave={!!disableSave} />
+        </Typography>
+      </Grid>
+    </Grid>
   );
 }
