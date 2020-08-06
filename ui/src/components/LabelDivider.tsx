01c8ee9e730fc9939c57a0d3b55846ae614f27c8
Gregory Chen <gregorychen3@gmail.com>
2020-08-06 12:42:10 -0400

    refactor labeldivider

--- ui/src/components/LabelDivider.tsx
+++ ui/src/components/LabelDivider.tsx
@@ -1,54 +1,32 @@
 import { makeStyles, Typography } from "@material-ui/core";
-import clsx from "clsx";
 import React from "react";
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    paddingTop: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
-  },
-  divider: {
-    borderBottomWidth: "1px",
-    borderBottomStyle: "solid",
-    borderBottomColor: theme.palette.divider,
-    lineHeight: 0,
-    marginBottom: theme.spacing(1),
-    marginTop: theme.spacing(1),
-  },
   label: {
-    paddingLeft: theme.spacing(1),
-    paddingRight: theme.spacing(1),
+    overflow: "hidden",
+    textAlign: "center",
+    "&::before, &::after": {
+      backgroundColor: theme.palette.divider,
+      content: '""',
+      display: "inline-block",
+      height: "1px",
+      position: "relative",
+      verticalAlign: "middle",
+      width: "50%",
+    },
+    "&::before": { right: theme.spacing(1), marginLeft: "-50%" },
+    "&::after": { left: theme.spacing(1), marginRight: "-50%" },
   },
-  defaultBg: { backgroundColor: theme.palette.background.default },
-  paperBg: { backgroundColor: theme.palette.background.paper },
 }));
 
 interface Props {
   label: string;
-  backgroundColor?: "paper" | "default";
 }
-export default function LabelDivider({ label, backgroundColor }: Props) {
+export default function LabelDivider({ label }: Props) {
   const classes = useStyles();
-  if (!backgroundColor) {
-    backgroundColor = "default";
-  }
   return (
-    <div className={classes.container}>
-      <Typography
-        variant="subtitle2"
-        color="textSecondary"
-        align="center"
-        className={classes.divider}
-      >
-        <span
-          className={clsx(classes.label, {
-            [classes.defaultBg]: backgroundColor === "default",
-            [classes.paperBg]: backgroundColor === "paper",
-          })}
-        >
-          {label}
-        </span>
-      </Typography>
-    </div>
+    <Typography variant="subtitle2" color="textSecondary" align="center" className={classes.label}>
+      {label}
+    </Typography>
   );
 }
