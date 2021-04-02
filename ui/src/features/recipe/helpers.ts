51b4b7bb330ee240dfbc6099a8a695d05ae65c6b
Gregory Chen <gregorychen3@gmail.com>
2021-04-02 11:46:57 -0400

    put getCourses back

--- ui/src/features/recipe/helpers.ts
+++ ui/src/features/recipe/helpers.ts
@@ -13,4 +13,5 @@ export const formatIngredient = (i: IIngredient, defaultServings: number, desire
   return ret;
 };
 
+export const getCourses = (recipes: IRecipe[]) => [...new Set<string>(recipes.map((r) => r.course))];
 export const getCuisines = (recipes: IRecipe[]) => [...new Set<string>(recipes.map((r) => r.cuisine))];
