04158e51102ff0f097f78c217bdd9a7cdc7b7409
Gregory Chen <gregorychen3@gmail.com>
2021-11-29 11:58:38 -0500

    RecipeHeader.tsx

--- ui/src/features/recipe/RecipeHeader.tsx
+++ ui/src/features/recipe/RecipeHeader.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography } from "@mui/material";
 import ActionMenu from "../../components/ActionMenu";
 
 interface Props {
@@ -9,14 +9,14 @@ interface Props {
 }
 export default function RecipeHeader({ title, onDelete, onEdit, disableSave }: Props) {
   return (
-    <Grid container direction="row" justify="space-between" alignItems="center" spacing={2}>
+    <Grid container direction="row" justifyContent="space-between" alignItems="center" spacing={2}>
       <Grid item xs={12} sm={12} md={2} />
       <Grid item xs={12} sm={12} md={8}>
         <Typography variant="h4" align="center">
           {title}
         </Typography>
       </Grid>
-      <Grid container item xs={12} sm={12} md={2} justify="center">
+      <Grid container item xs={12} sm={12} md={2} justifyContent="center">
         <Grid item>
           <Typography align="center" component="span">
             <ActionMenu onDelete={onDelete} onEdit={onEdit} disableSave={!!disableSave} />
