7e9c35c99f618bef3d71d3d37f128c8c4bfd2ff0
Gregory Chen <gregorychen3@gmail.com>
2021-11-29 21:26:12 -0500

    create recipe

--- ui/src/pages/CreateRecipePage.tsx
+++ ui/src/pages/CreateRecipePage.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { IRecipeModel } from "../../../src/db/recipe";
 import { IRecipe } from "../../../src/types";
+import { useApi } from "../apiClient";
 import RecipeForm from "../features/recipe/RecipeForm";
 import RecipeHeader from "../features/recipe/RecipeHeader";
-import { createRecipe } from "../features/recipe/RecipeSlice";
+import { putRecipe } from "../features/recipe/RecipeSlice";
 
 export default function RecipePage() {
   const d = useDispatch();
@@ -13,7 +15,15 @@ export default function RecipePage() {
   const [headerText, setHeaderText] = useState("");
 
   const handleRecipeEdited = (recipe: IRecipe) => setHeaderText(recipe.name);
-  const handleRecipeSaved = (recipe: IRecipe) => d(createRecipe({ recipe, history }));
+
+  const createRecipe = useApi<IRecipeModel>("POST", `/api/recipes`);
+  const handleRecipeSaved = (recipe: IRecipe) => {
+    const [call] = createRecipe(recipe);
+    call.then((resp) => {
+      d(putRecipe(resp.data));
+      history.push(`/recipes/${resp.data.id}`);
+    });
+  };
 
   return (
     <>
