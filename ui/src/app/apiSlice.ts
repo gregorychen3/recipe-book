33372ce78c0769225c55bc5492cda9025c9e5db4
Gregory Chen <gregorychen3@gmail.com>
2021-11-29 21:27:58 -0500

    remove unneeded code

--- ui/src/app/apiSlice.ts
+++ ui/src/app/apiSlice.ts
@@ -1,6 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 import _ from "lodash";
-import { createRecipe, deleteRecipe, fetchRecipe, fetchRecipes, updateRecipe } from "../features/recipe/RecipeSlice";
 import { RootState } from "./store";
 
 //
@@ -15,57 +14,6 @@ export const apiSlice = createSlice({
   name: "api",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(createRecipe.pending, (state, action) => {
-      state.inFlightRequests[action.meta.requestId] = {};
-    });
-    builder.addCase(createRecipe.fulfilled, (state, action) => {
-      delete state.inFlightRequests[action.meta.requestId];
-    });
-    builder.addCase(createRecipe.rejected, (state, action) => {
-      delete state.inFlightRequests[action.meta.requestId];
-    });
-
-    builder.addCase(fetchRecipes.pending, (state, action) => {
-      state.inFlightRequests[action.meta.requestId] = {};
-    });
-    builder.addCase(fetchRecipes.fulfilled, (state, action) => {
-      delete state.inFlightRequests[action.meta.requestId];
-    });
-    builder.addCase(fetchRecipes.rejected, (state, action) => {
-      delete state.inFlightRequests[action.meta.requestId];
-    });
-
-    builder.addCase(fetchRecipe.pending, (state, action) => {
-      state.inFlightRequests[action.meta.requestId] = {};
-    });
-    builder.addCase(fetchRecipe.fulfilled, (state, action) => {
-      delete state.inFlightRequests[action.meta.requestId];
-    });
-    builder.addCase(fetchRecipe.rejected, (state, action) => {
-      delete state.inFlightRequests[action.meta.requestId];
-    });
-
-    builder.addCase(updateRecipe.pending, (state, action) => {
-      state.inFlightRequests[action.meta.requestId] = {};
-    });
-    builder.addCase(updateRecipe.fulfilled, (state, action) => {
-      delete state.inFlightRequests[action.meta.requestId];
-    });
-    builder.addCase(updateRecipe.rejected, (state, action) => {
-      delete state.inFlightRequests[action.meta.requestId];
-    });
-
-    builder.addCase(deleteRecipe.pending, (state, action) => {
-      state.inFlightRequests[action.meta.requestId] = {};
-    });
-    builder.addCase(deleteRecipe.fulfilled, (state, action) => {
-      delete state.inFlightRequests[action.meta.requestId];
-    });
-    builder.addCase(deleteRecipe.rejected, (state, action) => {
-      delete state.inFlightRequests[action.meta.requestId];
-    });
-  },
 });
 
 export default apiSlice.reducer;
--- ui/src/features/recipe/RecipeSlice.ts
+++ ui/src/features/recipe/RecipeSlice.ts
@@ -1,54 +1,6 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { History } from "history";
-import { toast } from "react-toastify";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IRecipeModel } from "../../../../src/db/recipe";
-import { IRecipe } from "../../../../src/types";
-import apiClient from "../../apiClient";
 import { RootState } from "../../app/store";
-
-//
-// THUNKS
-// ------
-
-export const createRecipe = createAsyncThunk(
-  "recipe/createRecipe",
-  async (data: { recipe: IRecipe; history: History }) => {
-    const { recipe, history } = data;
-    const resp = await apiClient.createRecipe(recipe);
-    history.push(`/recipes/${resp.data.id}`);
-    return resp.data;
-  }
-);
-
-export const fetchRecipes = createAsyncThunk("recipe/fetchRecipes", async () => {
-  const resp = await apiClient.fetchRecipes();
-  return resp.data;
-});
-
-export const fetchRecipe = createAsyncThunk("recipe/fetchRecipe", async (recipeId: string) => {
-  const resp = await apiClient.fetchRecipe(recipeId);
-  return resp.data;
-});
-
-export const updateRecipe = createAsyncThunk(
-  "recipe/updateRecipe",
-  async (data: { recipeId: string; recipe: IRecipe; history: History }) => {
-    const { recipeId, recipe, history } = data;
-    const resp = await apiClient.updateRecipe(recipeId, recipe);
-    history.push(`/recipes/${recipeId}`);
-    return resp.data;
-  }
-);
-
-export const deleteRecipe = createAsyncThunk(
-  "recipe/deleteRecipe",
-  async (data: { recipeId: string; history: History }) => {
-    const { recipeId, history } = data;
-    const resp = await apiClient.deleteRecipe(recipeId);
-    history.push("/recipes");
-    return resp.data;
-  }
-);
 
 //
 // SLICE
@@ -70,50 +22,6 @@ export const recipeSlice = createSlice({
     removeRecipe: (state, { payload }: PayloadAction<string>) => {
       delete state.recipesById[payload];
     },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(createRecipe.fulfilled, (state, action) => {
-      const createdRecipe = action.payload;
-      state.recipesById[createdRecipe.id] = createdRecipe;
-      toast.success("Recipe successfully created");
-    });
-    builder.addCase(createRecipe.rejected, (_, action) => {
-      toast.error(`Failed to create recipe: ${action.error.message}`);
-    });
-
-    builder.addCase(fetchRecipes.fulfilled, (state, action) => {
-      action.payload.forEach((r) => {
-        state.recipesById[r.id] = r;
-      });
-    });
-    builder.addCase(fetchRecipes.rejected, (_, action) => {
-      toast.error(`Failed to fetch recipes: ${action.error.message}`);
-    });
-
-    builder.addCase(fetchRecipe.fulfilled, (state, action) => {
-      const recipe = action.payload;
-      state.recipesById[recipe.id] = recipe;
-    });
-    builder.addCase(fetchRecipe.rejected, (_, action) => {
-      toast.error(`Failed to fetch recipe: ${action.error.message}`);
-    });
-
-    builder.addCase(updateRecipe.fulfilled, (state, action) => {
-      const updatedRecipe = action.payload;
-      state.recipesById[updatedRecipe.id] = updatedRecipe;
-      toast.success("Recipe successfully updated");
-    });
-    builder.addCase(updateRecipe.rejected, (_, action) => {
-      toast.error(`Failed to update recipe: ${action.error.message}`);
-    });
-
-    builder.addCase(deleteRecipe.fulfilled, (state, action) => {
-      delete state.recipesById[action.payload.id];
-      toast.success("Recipe successfully deleted");
-    });
-    builder.addCase(deleteRecipe.rejected, (_, action) => {
-      toast.error(`Failed to delete recipe: ${action.error.message}`);
-    });
   },
 });
 
