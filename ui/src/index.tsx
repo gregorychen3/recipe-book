d77ee094c47f8b831eea865c1b134ac3206a3909
Gregory Chen <gregorychen3@gmail.com>
2022-12-19 00:18:21 -0500

    router

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { store } from "./app/store";
 import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root")!;
@@ -10,9 +11,11 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
