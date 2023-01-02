7d62ed97fca19029b2343082911ac10ff9f2a530
Gregory Chen <gregorychen3@gmail.com>
2023-01-01 23:04:43 -0500

    export db conn

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,16 +1,13 @@
+import { Client } from "pg";
 import logger from "../logger";
-import { Pool, Client } from "pg";
 
 const PG_CONN_STR = process.env.PG_CONN_STR || "";
 
 logger.info(`Connecting to postgres`);
 
-const pgClient = new Client({
+export const pgClient = new Client({
   connectionString: PG_CONN_STR,
   ssl: {
     rejectUnauthorized: false,
   },
 });
-
-const db = mongoose.connection;
-db.on("error", (err) => logger.error(`Failed connecting to mongodb: ${err}`));
