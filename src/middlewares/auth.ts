10f87994fc0f4e2732a9f11f1dacc52b9f503afa
Gregory Chen <gregorychen3@gmail.com>
2023-01-02 17:55:28 -0500

    fix compile error

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { NextFunction, Request, Response } from "express";
-import HttpException from "../errors";
+import { HttpException } from "../errors";
 
-const ADMIN_EMAILS: string[] = process.env.ADMIN_EMAILS ? JSON.parse(process.env.ADMIN_EMAILS) : [];
+const ADMIN_EMAILS: string[] = process.env.ADMIN_EMAILS
+  ? JSON.parse(process.env.ADMIN_EMAILS)
+  : [];
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
   const tokenId = req.header("Authorization");
@@ -11,10 +13,16 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
   }
 
   try {
-    const resp = await axios.get(`https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${tokenId}`);
+    const resp = await axios.get(
+      `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${tokenId}`
+    );
     const decoded = resp.data;
-    return ADMIN_EMAILS.includes(decoded.email) ? next() : next(new HttpException(401, `Unauthorized: not admin user`));
+    return ADMIN_EMAILS.includes(decoded.email)
+      ? next()
+      : next(new HttpException(401, `Unauthorized: not admin user`));
   } catch (e) {
-    return next(new HttpException(401, `Unauthorized: decoding tokenId: ${e.message}`));
+    return next(
+      new HttpException(401, `Unauthorized: decoding tokenId: ${e.message}`)
+    );
   }
 };
