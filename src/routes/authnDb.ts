17ad39472c2fa5c76ee36ced19687e39a8954dec
Gregory Chen <gregorychen3@gmail.com>
2023-01-08 05:20:05 -0500

    remove unneeded comment

--- src/routes/authnDb.ts
+++ src/routes/authnDb.ts
@@ -6,7 +6,6 @@ mkdirp.sync("./var/db");
 export const authnDb = new sqlite3.Database("./var/db/todos.db");
 
 authnDb.serialize(function () {
-  // create the database schema for the todos app
   authnDb.run(
     "CREATE TABLE IF NOT EXISTS users ( \
     id TEXT PRIMARY KEY, \
