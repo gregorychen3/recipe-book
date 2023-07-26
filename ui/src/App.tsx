72942788745ae3ff4f7ab2a33d087f71ea616662
Gregory Chen <gregorychen3@gmail.com>
2023-07-26 16:56:03 -0400

    refresh token on app mount and every hour

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,13 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@mui/material/Toolbar";
+import axios from "axios";
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { auth0Config } from "./auth0Config";
 import { AppHeader } from "./components/AppHeader";
 import { AboutPage } from "./pages/AboutPage";
 import { CreateRecipePage } from "./pages/CreateRecipePage";
@@ -13,6 +17,29 @@ import { RecipePage } from "./pages/RecipePage";
 import { RecipesPage } from "./pages/RecipesPage";
 
 export function App() {
+  const { getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    const ensureToken = () => {
+      getAccessTokenSilently({
+        authorizationParams: {
+          audience: `https://${auth0Config.domain}/api/v2/`,
+          scope: "read:current_user",
+        },
+      }).then((token) => {
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      });
+    };
+
+    ensureToken();
+
+    const interval = setInterval(() => {
+      ensureToken();
+    }, 3600);
+
+    return () => clearInterval(interval);
+  }, [getAccessTokenSilently]);
+
   return (
     <Box display="flex">
       <CssBaseline />
